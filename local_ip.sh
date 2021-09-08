8558d64b41fd553ce0b6f4c5d45d02e6e8925289
ganoes <karel.rozhon@etnetera.cz>
2021-09-08 12:36:50 +0200

    LAN in linux is determined based on the route priority

--- local_ip.sh
+++ local_ip.sh
@@ -2,12 +2,12 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     #running on Mac OS
-    INTERFACE=`netstat -nr | grep default | head -n 1| tr -s " " | cut -d " " -f 4 2>/dev/null`
+    INTERFACE=`netstat -nr | grep default | head -n 1| tr -s " " | cut -d " " -f 4 `
     IP=`ifconfig $INTERFACE 2>/dev/null | grep "inet " | tr -s " " | cut -d" " -f2`
     echo $IP 2>/dev/null
 else
     #running on Linux
-    INTERFACE=`netstat -nr | tail -n +3 | head -n 1 | tr -s " " | cut -d" " -f8 2>/dev/null`
+    INTERFACE=`netstat -nre | tail -n +3 | tr -s " " | cut -d " " -f 5,8 | sort -n | uniq | cut -d " " -f2 | head -1`
     IP=`ip addr show dev $INTERFACE  2>/dev/null | grep "inet " | tr -s " " | cut -d " " -f 3 | cut -d "/" -f1`
     echo $IP 2>/dev/null
 fi
