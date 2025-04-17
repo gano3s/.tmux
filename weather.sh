4fff7e8c614dec2596ac84bd027e78527920d334
Karel K. Rozhoň <gano3s@protonmail.com>
2025-04-17 14:10:39 +0200

    Updated weather script

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 METEO=$(curl http://meteostanice.knih-pi.cz/xml.xml 2>/dev/null)
-WPI_TEMP=$(echo $METEO | xmllint --format - | grep -i 1006 -A 4 | grep value | cut -d "<" -f 2 | cut -d ">" -f 2)
+WPI_TEMP=$(echo $METEO | xmllint --format - | grep -i 1006 -A 4 | grep -v "<value>1006" | grep value | cut -d "<" -f 2 | cut -d ">" -f 2)
 WPI_HUMIDITY=$(echo $METEO | xmllint --format - | grep -i 1007 -A 4 | grep -v "<value>1007" | grep value | cut -d "<" -f 2 | cut -d ">" -f 2)
 echo "Temp: $WPI_TEMP, Humidity: $WPI_HUMIDITY"
